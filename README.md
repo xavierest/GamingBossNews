289aefad791db4839e518aea1f65b70fc7ec257a
Xavier <xavier.carmona.estrada@gmail.com>
2023-01-29 17:26:19 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GamingBossNews
 # NewsSite
+# NewsSite
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(function() {
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("toggle");
+    });
+    $("#registerButton").click(function(){ 
+        $("#registerModal").modal("toggle");
+    });
+});
